f4b4d93bf951ad6ca29e1057b1b4ae8aa16ff1b9
realko0323-hub <godteo0323@gmail.com>
2025-11-26 19:07:25 +0900

    Add files via upload

---
+++ HW7/main.py
@@ -0,0 +1,10 @@
+
+import calculate
+res =  calculate.plus(5,4)
+print(res)
+ans = calculate.minus(5,4)
+print(ans)
+prod = calculate.multiply(5,4)
+print(prod)
+quot = calculate.divide(5,4)
+print(quot)
